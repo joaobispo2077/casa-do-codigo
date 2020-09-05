a97a3a25ed85892d889112a8708009e485ecbdc0
João Bispo <vitor.vi10@hotmail.com>
2020-09-05 15:04:10 -0300

    :paperclip: chore: old test routes

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 const http = require('http');
 
 const server = http.createServer(function(request, response) {
-    response.end(`
+
+    let html = '';
+    if (request.url == '/') {
+        html = `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Hello World </h1>
+                </body> 
+            </html>
+          `;
+    } else if (request.url == '/livros') {
+        html = `
         <html>
             <head>
                 <meta charset="utf-8">
             </head>
             <body>
-                <h1> Hello World </h1>
+                <h1> Listagem de Livros </h1>
             </body> 
         </html>
-        `)
+      `;
+    }
+    response.end(html)
 });
-
 server.listen(3000);
