487bec439fa26dc1bc6b50b1652f1380e160a9b3
João Bispo <vitor.vi10@hotmail.com>
2020-09-05 16:27:34 -0300

    :paperclip: chore: using express on routes

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer(function(request, response) {
+const app = express();
 
-    let html = '';
-    if (request.url == '/') {
-        html = `
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1> Hello World </h1>
-                </body> 
-            </html>
-          `;
-    } else if (request.url == '/livros') {
-        html = `
+app.listen(3000, function() {
+    console.log(`servidor rodando na porta 3000`);
+});
+
+app.get('/', function(request, response) {
+    response.send(`
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Hello World </h1>
+            </body> 
+        </html>
+    `);
+});
+
+app.get('/livros', function(request, response) {
+    response.send(`
         <html>
             <head>
                 <meta charset="utf-8">
@@ -24,8 +29,36 @@ const server = http.createServer(function(request, response) {
                 <h1> Listagem de Livros </h1>
             </body> 
         </html>
-      `;
-    }
-    response.end(html)
+    `);
 });
-server.listen(3000);
+// const http = require('http');
+
+// const server = http.createServer(function(request, response) {
+
+//     let html = '';
+//     if (request.url == '/') {
+//         html = `
+//             <html>
+//                 <head>
+//                     <meta charset="utf-8">
+//                 </head>
+//                 <body>
+//                     <h1> Hello World </h1>
+//                 </body> 
+//             </html>
+//           `;
+//     } else if (request.url == '/livros') {
+//         html = `
+//         <html>
+//             <head>
+//                 <meta charset="utf-8">
+//             </head>
+//             <body>
+//                 <h1> Listagem de Livros </h1>
+//             </body> 
+//         </html>
+//       `;
+//     }
+//     response.end(html)
+// });
+// server.listen(3000);
